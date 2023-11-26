ee54d5873e08438591aef253009c8f3d8f9e9038
PedroHIS <pedro.isidorio.soares@gmail.com>
2023-11-26 19:44:47 -0300

    feat: add playRound function

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function getPlayerChoice() {
     const userChoice = prompt('Choose an option (rock, paper, or scissors):');
     return options.includes(userChoice.toLowerCase()) ? userChoice.toLowerCase() : getPlayerChoice();
 }
+
+function playRound(playerSelection, computerSelection) {
+    /*
+    Taking advantage of the array organization. If the computer's option is different from the player's and
+    is immediately to the right of the player's option, then the player loses:
+    Case 1: [rock(Player) -> paper(Computer) -> scissors]
+    If it's immediately to the left, then the player wins:
+    Case 2: [rock <- paper(Computer) <- scissors(Player)]
+    */
+    const options = ['rock', 'paper', 'scissors'];
+    const indexPlayer = options.indexOf(playerSelection);
+    
+    if (playerSelection === computerSelection) {
+        return [0, `It's a tie! Both chose ${playerSelection}`];
+    }
+    
+    let correctionLastElement = 0;
+    if (playerSelection === 'scissors') {
+        correctionLastElement = 3;
+    }
+
+    const rightElement = options[(indexPlayer + 1) - correctionLastElement];
+    if (computerSelection === rightElement) {
+        return [1, `You Lose! ${computerSelection} beats ${playerSelection}`];
+    }
+    return [2, `You Win! ${playerSelection} beats ${computerSelection}`];
+}
